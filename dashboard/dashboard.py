ef99cdba1922a8e16a883b37d54b9eae5ed9900b
Maissa Dridi <maissadridi1@gmail.com>
2024-05-02 15:50:30 +0100

    dashboarding

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,27 +1,20 @@
 import pandas as pd
 import plotly.graph_objs as go
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 
-# Supposons que vous avez déjà chargé vos données dans un DataFrame pandas appelé df
-df = pd.read_csv('dashboard\\histo_cotation.csv')  # ou df = pd.read_csv(r'dashboard\histo_cotation.csv')
-
+# Charger les données depuis un fichier CSV
+df = pd.read_csv('dashboard\histo_cotation.csv')
 # Convertir les colonnes pertinentes en types de données numériques
-columns_to_convert = ['QUANTITE_NEGOCIEE', 'CAPITAUX']  # Ajoutez d'autres colonnes si nécessaire
+columns_to_convert = ['QUANTITE_NEGOCIEE', 'CAPITAUX', 'NB_TRANSACTION']  
 for column in columns_to_convert:
     df[column] = pd.to_numeric(df[column], errors='coerce')
-
-columns_to_convert = ['NB_TRANSACTION']  # Ajoutez d'autres colonnes si nécessaire
-for column in columns_to_convert:
-    df[column] = pd.to_numeric(df[column], errors='coerce')
-
 # Calcul des KPIs
-moyenne_valeur = df['QUANTITE_NEGOCIEE'].mean()
-ecart_type_valeur = df['QUANTITE_NEGOCIEE'].std()
-nb_transactions_total = df['NB_TRANSACTION'].sum()
-capitaux_total = df['CAPITAUX'].sum()
+total_transactions = df['NB_TRANSACTION'].sum()
+total_capitaux = df['CAPITAUX'].sum()
+nombre_entreprises = df['VALEUR'].nunique()
 
 # Initialisation de l'application Dash
 app = dash.Dash(__name__)
@@ -31,28 +24,56 @@ app.layout = html.Div(children=[
     html.H1(children='Tableau de bord'),
 
     html.Div(children='''
-        KPIs de l'entreprise:
+        Graphiques basés sur les données de l'entreprise :
     '''),
 
+    # Graphique Pie basé sur les valeurs de la colonne 'VALEUR'
+    dcc.Graph(
+        id='graph-pie',
+        figure={
+            'data': [
+                go.Pie(
+                    labels=df['VALEUR'].value_counts().index.tolist(),
+                    values=df['VALEUR'].value_counts().values.tolist()
+                )
+            ],
+            'layout': {
+                'title': 'Répartition des valeurs'
+            }
+        }
+    ),
+
+    # Graphique Bar basé sur les valeurs de la colonne 'SEANCE'
+    dcc.Graph(
+        id='graph-bar',
+        figure={
+            'data': [
+                go.Bar(
+                    x=df['SEANCE'],
+                    y=df['QUANTITE_NEGOCIEE']
+                )
+            ],
+            'layout': {
+                'title': 'Quantité négociée par séance'
+            }
+        }
+    ),
+
+    # KPIs
     html.Div(children=[
         html.Div(children=[
-            html.H3('Moyenne de la valeur'),
-            html.H4(f'{moyenne_valeur:.2f}')
-        ], className='kpi'),
-
-        html.Div(children=[
-            html.H3("Écart-type de la valeur"),
-            html.H4(f'{ecart_type_valeur:.2f}')
-        ], className='kpi'),
-
-        html.Div(children=[
             html.H3('Nombre total de transactions'),
-            html.H4(f'{nb_transactions_total}')
+            html.H4(total_transactions)
         ], className='kpi'),
 
         html.Div(children=[
             html.H3('Total des capitaux'),
-            html.H4(f'{capitaux_total}')
+            html.H4(f'${total_capitaux}')
+        ], className='kpi'),
+
+        html.Div(children=[
+            html.H3('Nombre d\'entreprises'),
+            html.H4(nombre_entreprises)
         ], className='kpi')
     ], className='kpi-container')
 ])
